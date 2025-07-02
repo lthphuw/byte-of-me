494266415145ed0f723d682b2b5b1429895f84e2
lthphuw <lthphuw@gmail.com>
2025-07-03 00:30:09 +0700

    minor: update robots

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -1,10 +1,14 @@
-import { MetadataRoute } from 'next';
+import type { MetadataRoute } from 'next';
+
+import { host } from '@/config/host';
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: '/private/',
     },
+    sitemap: `${host}/sitemap.xml`,
   };
 }
